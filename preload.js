c162aa3085f7b489e3a601bf80cd3dd8b84a3c99
rahimozkan-boop <rahimozkan@gmail.com>
2026-09-12 00:52:16 +0300

    feat: expose secure AI builder IPC bridge

--- preload.js
+++ preload.js
@@ -2,5 +2,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('desktopBuilder', {
   listProjects: () => ipcRenderer.invoke('projects:list'),
   saveProject: project => ipcRenderer.invoke('project:save', project),
-  openProject: () => ipcRenderer.invoke('dialog:open-project')
+  openProject: () => ipcRenderer.invoke('dialog:open-project'),
+  getAISettings: () => ipcRenderer.invoke('ai:get-settings'),
+  setAISettings: settings => ipcRenderer.invoke('ai:set-settings', settings),
+  checkAI: settings => ipcRenderer.invoke('ai:health', settings),
+  generate: (prompt, settings) => ipcRenderer.invoke('ai:generate', { prompt, settings })
 });
